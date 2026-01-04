e41940393ccc8b396c3090a036d931aed8ec553d
Ander2211 <anderhdz06@gmail.com>
2026-01-03 21:57:17 -0600

    Recive el JSON de API-REST-con-C-

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useFetch } from "./events/useFetch";
 import "./App.css";
 
 function App() {
-  const { data, loading,error } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
+  const { data, loading, error } = useFetch(
+    "http://localhost:5298/api/v1/producto"
   );
 
   return (
@@ -11,31 +11,50 @@ function App() {
       <h1 className="text-xl">API REST</h1>
       <div className="container">
         {error && <div>error</div>}
+
         {loading && (
           <div className="m-4 size-5 animate-spin" viewBox="0 0 24 24">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth={1.5}
               stroke="currentColor"
-              class="size-6"
+              className="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
               />
             </svg>
           </div>
         )}
-        <ul>
-          {data?.map((user) => (
-            <li key={user.id}>
-              {user.name} {user.email}{" "}
-            </li>
-          ))}
-        </ul>
+
+        {data && data.length > 0 ? (
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Producto</th>
+                  <th>Precio</th>
+                  <th>Stock</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((producto) => (
+                  <tr key={producto.id}>
+                    <td>{producto.nombre}</td>
+                    <td>{producto.precio}</td>
+                    <td>{producto.stock}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          !loading && <div>No hay productos</div>
+        )}
       </div>
     </div>
   );
